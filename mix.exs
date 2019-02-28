a6e3491ca9bcc8ac60f9dfbae38f535cb25d760a
Jake Prem <jakeprem@gmail.com>
2019-02-28 15:00:35 -0500

    SMRT-977 Bump version to 0.2.0
    co-authored-by: Mihail Chirita <mihailrc@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SCOS.MixProject do
   def project do
     [
       app: :scos_ex,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
