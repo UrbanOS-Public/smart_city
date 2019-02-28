0cebdf034c0ce4a1eed0a9396f1948dead9bf45d
Jake Prem <jakeprem@gmail.com>
2019-02-28 14:58:49 -0500

    SMRT-977 Finish DataMessage and add tests
    co-authored-by: Mihail Chirita <mihailrc@gmail.com>

--- lib/scos/data_message.ex
+++ lib/scos/data_message.ex
@@ -4,9 +4,14 @@ defmodule SCOS.DataMessage do
   """
 
   alias SCOS.DataMessage
+  alias SCOS.DataMessage.Timing
 
   @enforce_keys [:dataset_id, :payload, :_metadata, :operational]
   defstruct _metadata: %{org: nil, name: nil, stream: false}, dataset_id: nil, payload: nil, operational: %{timing: []}
+
+  def new(opts) do
+    struct!(__MODULE__, opts)
+  end
 
   def parse_message(value) when is_binary(value) do
     value
@@ -21,17 +26,18 @@ defmodule SCOS.DataMessage do
   end
 
   def add_timing(%__MODULE__{operational: %{timing: timing}} = message, %DataMessage.Timing{} = new_timing) do
-    put_in_operational(message, timing, [new_timing | timing])
+    case Timing.validate(new_timing) do
+      {:ok, new_timing} -> put_in_operational(message, :timing, [new_timing | timing])
+      {:error, errors} -> raise ArgumentError, "Invalid Timing: #{errors}"
+    end
+
+    put_in_operational(message, :timing, [new_timing | timing])
   end
 
   def get_all_timings(%__MODULE__{operational: %{timing: timing}}), do: timing
 
   # Private functions
-  defp put_in_operational(%__MODULE__{} = message, key, value) do
-    put_in(message, [:operational, key], value)
-  end
-
-  def new(opts) do
-    struct!(__MODULE__, opts)
+  defp put_in_operational(%__MODULE__{operational: operational} = message, key, value) do
+    %{message | operational: Map.put(operational, key, value)}
   end
 end
--- test/scos/data_message_test.exs
+++ test/scos/data_message_test.exs
@@ -3,16 +3,86 @@ defmodule SCOS.DataMessageTest do
   doctest SCOS.DataMessage
 
   alias SCOS.DataMessage
+  alias SCOS.DataMessage.Timing
 
   describe "parse_message" do
+    test "throws error without required keys" do
+      value = "{}"
+
+      assert_raise ArgumentError, fn -> DataMessage.parse_message(value) end
+    end
+
+    test "creates a DataMessage struct with required keys" do
+      value = "{\"_metadata\":[],\"dataset_id\":\"abc\",\"operational\":{},\"payload\":\"whatever\"}"
+
+      assert %DataMessage{
+               dataset_id: "abc",
+               payload: "whatever",
+               _metadata: [],
+               operational: %{}
+             } = DataMessage.parse_message(value)
+    end
   end
 
   describe "encode_message" do
+    test "encodes to JSON" do
+      data_message = %DataMessage{
+        dataset_id: "abc",
+        payload: "whatever",
+        _metadata: [],
+        operational: %{}
+      }
+
+      assert DataMessage.encode_message(data_message) ==
+               "{\"_metadata\":[],\"dataset_id\":\"abc\",\"operational\":{},\"payload\":\"whatever\"}"
+    end
   end
 
   describe "add_timing" do
+    test "throws error with invalid timing" do
+      invalid_timing = Timing.new(app: "whatever", label: "whatever")
+
+      message =
+        DataMessage.new(dataset_id: "whatever", payload: "whatever", _metadata: "whatever", operational: %{timing: []})
+
+      assert_raise ArgumentError, fn -> DataMessage.add_timing(message, invalid_timing) end
+    end
+
+    test "adds valid timing" do
+      valid_timing =
+        Timing.new(
+          app: "whatever",
+          label: "whatever",
+          start_time: Timing.current_time(),
+          end_time: Timing.current_time()
+        )
+
+      message =
+        DataMessage.new(dataset_id: "whatever", payload: "whatever", _metadata: "whatever", operational: %{timing: []})
+
+      assert %DataMessage{operational: %{timing: [^valid_timing]}} = DataMessage.add_timing(message, valid_timing)
+    end
   end
 
   describe "get_all_timings" do
+    test "returns timings" do
+      timing =
+        Timing.new(
+          app: "whatever",
+          label: "whatever",
+          start_time: Timing.current_time(),
+          end_time: Timing.current_time()
+        )
+
+      message =
+        DataMessage.new(
+          dataset_id: "whatever",
+          payload: "whatever",
+          _metadata: "whatever",
+          operational: %{timing: [timing]}
+        )
+
+      assert [^timing] = DataMessage.get_all_timings(message)
+    end
   end
 end
