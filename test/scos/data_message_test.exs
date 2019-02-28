d8ce10029f2b60d6ff9068262e1e428a2b7e8dce
Jake Prem <jakeprem@gmail.com>
2019-02-28 14:20:18 -0500

    SMRT-977 Remove outdated tests and unused files
    co-authored-by: Mihail Chirita <mihailrc@gmail.com>

---
+++ test/scos/data_message_test.exs
@@ -0,0 +1,18 @@
+defmodule SCOS.DataMessageTest do
+  use ExUnit.Case
+  doctest SCOS.DataMessage
+
+  alias SCOS.DataMessage
+
+  describe "parse_message" do
+  end
+
+  describe "encode_message" do
+  end
+
+  describe "add_timing" do
+  end
+
+  describe "get_all_timings" do
+  end
+end
