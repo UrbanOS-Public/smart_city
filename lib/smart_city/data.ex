72c9970f3c1e13759fa55bb82632db02d91d0446
Jake Prem <jakeprem@gmail.com>
2019-05-03 10:12:24 -0400

    Revert "Merge pull request #5 from SmartColumbusOS/kafka_key"
    This reverts commit 8be9c0751b8f61a5f990d13bfc2d297ff82a912d, reversing
    changes made to c6a219d76c704a6cb3a22d76d3ee27f18b6eb8cf.

--- lib/smart_city/data.ex
+++ lib/smart_city/data.ex
@@ -12,7 +12,6 @@ defmodule SmartCity.Data do
   defstruct version: "0.1",
             _metadata: %{org: nil, name: nil, stream: false},
             dataset_id: nil,
-            kafka_key: nil,
             payload: nil,
             operational: %{timing: []}
 
@@ -47,14 +46,13 @@ defmodule SmartCity.Data do
     |> new()
   end
 
-  def new(%{dataset_id: dataset_id, operational: operational, payload: payload, _metadata: metadata} = data) do
+  def new(%{dataset_id: dataset_id, operational: operational, payload: payload, _metadata: metadata}) do
     timings = Map.get(operational, :timing, [])
 
     struct =
       struct(__MODULE__, %{
         dataset_id: dataset_id,
         payload: payload,
-        kafka_key: Map.get(data, :kafka_key, nil),
         _metadata: metadata,
         operational: %{operational | timing: Enum.map(timings, &Timing.new/1)}
       })
