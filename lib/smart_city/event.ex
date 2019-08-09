0039d27d5d533a25d258d242622d2980d1ece0eb
jeffgrunewald <jeff@grunewalddesign.com>
2019-08-08 23:59:22 -0400

    File Upload new/1 and refactor events for consistent naming

--- lib/smart_city/event.ex
+++ lib/smart_city/event.ex
@@ -1,4 +1,4 @@
-defmodule SmartCity.Events do
+defmodule SmartCity.Event do
   @moduledoc """
   Defines macros for encoding event types the Smart City platform
   will respond to in any of the various micro service components
@@ -9,17 +9,17 @@ defmodule SmartCity.Events do
   Defines an update event to a dataset within the system. The
   system treats create events as a subset of updates.
   """
-  defmacro update_dataset(), do: "dataset:update"
+  defmacro dataset_update(), do: quote(do: "dataset:update")
 
   @doc """
   Defines an update event to an organization within the system.
   The system treats create events as a subset of updates.
   """
-  defmacro update_organization(), do: "organization:update"
+  defmacro organization_update(), do: quote(do: "organization:update")
 
   @doc """
   Signals that a new file has been uploaded to the object store
   and made available for the rest of the system.
   """
-  defmacro file_uploaded(), do: "file:uploaded"
+  defmacro file_upload(), do: quote(do: "file:upload")
 end
