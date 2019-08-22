8ca7c2d1c54f6d11b56bd855c5bbab620787d472
jmorris-pillar <jmorris2@pillartechnology.com>
2019-08-22 16:12:38 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "2.4.2",
+      version: "2.5.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
