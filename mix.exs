ff369a0bca72fda921ddfe1a4ad6d6e87f3b02eb
cameronmarsh <cmarsh@pillartechnology.com>
2019-08-27 15:14:28 -0400

    bump version for release
    smartcitiesdata/forklift#94
    
    co-authored-by: Benjamin Brewer <benjamin.brewer@accenture.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.Data.MixProject do
   def project do
     [
       app: :smart_city_data,
-      version: "3.0.0",
+      version: "3.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
