c4d3427be133d5631da1a5db801767a6fa292db9
bbalser <bbalser@pillartechnology.com>
2019-09-04 07:45:08 -0400

    working on reaper refactor

--- lib/smart_city/event.ex
+++ lib/smart_city/event.ex
@@ -18,8 +18,28 @@ defmodule SmartCity.Event do
   defmacro organization_update(), do: quote(do: "organization:update")
 
   @doc """
+  Signals to file should be downloaded
+  """
+  defmacro hosted_file_start(), do: quote(do: "hosted:file:start")
+
+  @doc """
+  Hosted file and been downloaded and stored
+  """
+  defmacro hosted_file_complete(), do: quote(do: "hosted:file:complete")
+
+  @doc """
   Signals that a new file has been uploaded to the object store
   and made available for the rest of the system.
   """
   defmacro file_upload(), do: quote(do: "file:upload")
+
+  @doc """
+  Signals that a dataset extraction is starting
+  """
+  defmacro dataset_extract_start(), do: quote(do: "dataset:extract:start")
+
+  @doc """
+  Signals that dataset extraction has completed
+  """
+  defmacro dataset_extract_complete(), do: quote(do: "dataset:extract:complete")
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "2.7.0",
+      version: "2.7.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
