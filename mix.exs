4746f39fcc10ca91db6964d43552e19176dc8042
Jessie Morris <jmorris2@pillartechnology.com>
2019-10-23 10:49:22 -0400

    Increasing minor version to package new events and modified date changes
    This fixes previously non-backwards compatible modified date validation

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "3.2.4",
+      version: "3.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
