a99cba956a74124503ba366e4d79f100b9032528
Christine (Walsh) Poydence <73911735+christinepoydence@users.noreply.github.com>
2021-07-08 12:16:24 -0400

    major version increase (#111)
    Datastillery/smartcitiesdata#1255
    
    co-authored-by: Christine Walsh <christine.poydence@gmail.com>
    Co-authored-by: Nicole Arruda <narruda789@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "3.25.1",
+      version: "4.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
