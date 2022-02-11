6fc9f5b98a7ae8c67b21ee945ce31c05aebce1dc
Christine (Walsh) Poydence <73911735+christinepoydence@users.noreply.github.com>
2022-02-11 15:29:38 -0500

    add back dataset fields (#117)
    Datastillery/internal#521
    
    Co-authored-by: Christine Walsh <christine.poydence@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "5.0.3",
+      version: "5.0.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
