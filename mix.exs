698fe6efa440a50f05945ba0b74e7761aac1d91b
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-08-22 18:27:00 -0400

    Adding transformation id and sequence

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.MixProject do
   def project do
     [
       app: :smart_city,
-      version: "5.2.4",
+      version: "5.2.5",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
