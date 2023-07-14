8bcfe66454abf701dd1f99a56efd6f8675d3a7fa
Ian Abbott <ij.abbott2306@gmail.com>
2023-07-14 16:32:51 -0500

    add event long struct

---
+++ lib/smart_city/event_log.ex
@@ -0,0 +1,83 @@
+defmodule SmartCity.EventLog do
+  require Logger
+
+  alias SmartCity.Helpers
+
+  @moduledoc """
+  Struct defining an event log event.
+
+  ```javascript
+  const EventLog = {
+    "title": "",
+    "timestamp", "",
+    "source": "",
+    "description": "",
+    "dataset_id": ""
+  }
+  ```
+  """
+  @type not_required(type) :: type | nil
+
+  @type t :: %SmartCity.EventLog{
+               title: String.t(),
+               timestamp: String.t(),
+               source: String.t(),
+               description: String.t(),
+               dataset_id: not_required(String.t()) # Making dataset_id not required under the assumption that event log may be used for events not associated with a dataset (like organization updates)
+             }
+
+  @derive Jason.Encoder
+  defstruct title: nil,
+            timestamp: nil,
+            source: nil,
+            description: nil,
+            dataset_id: nil
+
+  use Accessible
+
+  @doc """
+  Returns a new `SmartCity.EventLog`.
+  Can be created from `Map` with string or atom keys.
+  Raises an `ArgumentError` when passed invalid input
+
+  ## Parameters
+
+  - msg: Map with string or atom keys that defines the event log metadata
+
+  Required Keys:
+      - title
+      - timestamp
+      - source
+      - description
+      - dataset_id
+  """
+
+  @spec new(map()) :: SmartCity.EventLog.t()
+  def new(%{"title" => _} = msg) do
+    msg
+    |> Helpers.to_atom_keys()
+    |> new()
+  end
+
+  @spec new(map()) :: SmartCity.EventLog.t()
+  def new(
+        %{
+          title: title,
+          timestamp: timestamp,
+          source: source,
+          description: description
+        } = msg
+      ) do
+    create(msg)
+  end
+
+  def new(msg) do
+    raise ArgumentError, "Invalid event log metadata: #{inspect(msg)}"
+  end
+
+  defp create(%__MODULE__{} = struct) do
+    struct |> Map.from_struct() |> create()
+  end
+
+  defp create(map), do: struct(%__MODULE__{}, map)
+end
---
+++ test/smart_city/event_log_test.exs
@@ -0,0 +1,96 @@
+defmodule SmartCity.EventLogTest do
+  use ExUnit.Case
+  import Checkov
+
+  alias SmartCity.EventLog
+
+  setup do
+    message = %{
+      "title" => "MyTitle",
+      "timestamp" => "myTimestamp",
+      "source" => "mySource",
+      "description" => "myDescription",
+      "dataset_id" => "myDatasetId"
+    }
+
+    {:ok, message: message}
+  end
+
+  describe "new/1" do
+    test "returns EventLog struct" do
+      actual =
+        EventLog.new(%{
+          title: "MyTitle",
+          timestamp: "myTimestamp",
+          source: "mySource",
+          description: "myDescription",
+          dataset_id: "myDatasetId"
+        })
+
+      assert actual.title == "MyTitle"
+      assert actual.timestamp == "myTimestamp"
+      assert actual.source == "mySource"
+      assert actual.description == "myDescription"
+      assert actual.dataset_id == "myDatasetId"
+    end
+
+    test "a struct with additional fields is cleaned" do
+      actual =
+        EventLog.new(%{
+          title: "MyTitle",
+          timestamp: "myTimestamp",
+          source: "mySource",
+          description: "myDescription",
+          dataset_id: "myDatasetId",
+          is_a_good_struct: "no"
+        })
+
+      assert actual.title == "MyTitle"
+      assert actual.timestamp == "myTimestamp"
+      assert actual.source == "mySource"
+      assert actual.description == "myDescription"
+      assert actual.dataset_id == "myDatasetId"
+      assert not Map.has_key?(actual, :is_a_good_struct)
+    end
+
+    data_test "field #{field} has a default value of #{inspect(default)}" do
+      actual =
+        EventLog.new(%{
+          title: "",
+          timestamp: "",
+          source: "",
+          description: "",
+          dataset_id: "",
+        })
+
+      assert Map.get(actual, field) == default
+
+      where(
+        field: [:title, :timestamp, :source, :description, :dataset_id],
+        default: ["", "", "", "", ""]
+      )
+    end
+
+    test "returns Ingestion struct when given string keys", %{message: msg} do
+      actual = EventLog.new(msg)
+      assert actual.title == "MyTitle"
+      assert actual.timestamp == "myTimestamp"
+      assert actual.source == "mySource"
+      assert actual.description == "myDescription"
+      assert actual.dataset_id == "myDatasetId"
+    end
+
+    data_test "throws error when creating EventLog struct without required field: #{field}", %{message: msg} do
+      assert_raise ArgumentError, fn -> EventLog.new(msg |> Map.delete(field)) end
+
+      where(field: ["title", "timestamp", "source", "description"])
+    end
+  end
+
+  describe "struct" do
+    test "can be encoded to JSON", %{message: message} do
+      json = Jason.encode!(message)
+      assert is_binary(json)
+    end
+  end
+end
